acc193ca8ff1cd643483486ade6156c8a19f4eb8
Harrison <harrison_0418@outlook.com>
2025-12-16 15:32:08 +0800

    modify script for local test

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-docker run -p 8501:8501 --env-file ../.env simple-chatbot:latest
+export PULUMI_CONFIG_PASSPHRASE=''
+APP_NAME="ai-chatbot-app"
+CLOUDFRONT_STATIC_URL="$(pulumi --cwd ../infra stack output assets_base_url)"
+docker run -p 8501:8501 \
+    -e CLOUDFRONT_STATIC_URL="${CLOUDFRONT_STATIC_URL}" \
+    --env-file ../.env \
+    "${APP_NAME}":latest
